561788ee649e5a24784ebf437fd935e99e47e3ca
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-07-01 15:15:09 +0530

    Add @CrossOrigin("*")

--- src/main/java/sales/savvy/controller/ProductController.java
+++ src/main/java/sales/savvy/controller/ProductController.java
@@ -3,6 +3,7 @@ package sales.savvy.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import sales.savvy.entity.Product;
 import sales.savvy.service.ProductService;
 
+@CrossOrigin("*")
 @RestController
 public class ProductController {
 
--- src/main/java/sales/savvy/controller/UserController.java
+++ src/main/java/sales/savvy/controller/UserController.java
@@ -1,6 +1,7 @@
 package sales.savvy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import sales.savvy.dto.LoginData;
 import sales.savvy.entity.User;
 import sales.savvy.service.UserService;
 
+@CrossOrigin("*")
 @RestController
 public class UserController {
 
